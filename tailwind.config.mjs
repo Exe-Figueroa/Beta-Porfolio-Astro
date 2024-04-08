8abcac7ad10ac884ecc0539ee3c9bfce7e0b2d0f
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-04-08 14:33:55 -0300

    Refactor header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily:{
+				'goblin': ["Permanent Marker", 'cursive', 'sans-serif'],
+				'custom': ["Poppins", 'sans-serif']
+			}
+		},
 	},
 	plugins: [],
 }
