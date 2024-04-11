a372ceb2df67a928b526568036e86a5b2e7b40e5
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-04-10 21:03:04 -0300

    Technologies section created

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,22 @@ export default {
 			fontFamily:{
 				'goblin': ["Permanent Marker", 'cursive', 'sans-serif'],
 				'custom': ["Poppins", 'sans-serif']
+			},
+			backgroundColor: {
+				'node': '#346f18',
+				'js': '#f7e04f',
+				'css': '#3177b9',
+				'html': '#e55138',
+				'postgres': '#478fca',
+				'react': '#61d2f1',
+			},
+			colors: {
+				'node': '#346f18',
+				'js': '#f7e04f',
+				'css': '#3177b9',
+				'html': '#e55138',
+				'postgres': '#478fca',
+				'react': '#61d2f1',
 			}
 		},
 	},
